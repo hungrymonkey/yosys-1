61a9d039af3dedde125b8cd6592c2a205f484040
boringmonkey@gmail.com <boringmonkey@gmail.com>
2016-03-05 08:24:42 -0800

    still not working

--- passes/techmap/dff2lut.cc
+++ passes/techmap/dff2lut.cc
@@ -221,7 +221,7 @@ struct Dff2lutWorker
 				sig_master_a.append( dff_cell->getPort("\\CLK") ); 
 
 				sig_slave_a.append( sig_master_y ); 
-				sig_slave_a.append( make_patterns_logic(it.first, false) ); 
+				sig_slave_a.append( new_sig_q ); 
 				sig_slave_a.append( dff_cell->getPort("\\CLK") ); 
 				RTLIL::Cell *new_cell_master = module->addLut(NEW_ID, sig_master_a, sig_master_y, RTLIL::Const::Const("11001010"));
 				RTLIL::Cell *new_cell_slave = module->addLut(NEW_ID, sig_slave_a, new_sig_q, RTLIL::Const::Const("11001010"));
